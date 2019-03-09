df2d3a7913a4123cad68c84f7a43e59825a0776a
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-03-09 15:35:22 +0700

    Update List.js

--- src/Component/List.js
+++ src/Component/List.js
@@ -1,78 +1,72 @@
-import React, { Component } from 'react';
-import Word from './Word.js';
-import Form from './Form.js';
-import Filter from './Filter.js';
+import React, { Component } from 'react'
+import Word from './Word';
+import Form from './Form';
+import Filter from './Filter';
 const words = [
-    {id : 'a1' ,  en : 'One' , vn : 'Một' , isMemorized : true},
-    {id : 'a2' ,  en : 'Two' , vn : 'Hai' , isMemorized : false},
-    {id : 'a3' ,  en : 'Three' , vn : 'Ba' , isMemorized : true},
-  ]
-
-export default class List extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            words : words,
-            txtEn : '',
-            txtVn : '',
-            shouldShowForm : false,
-            filterMode : 'Show_All'
-        }
-        this.onaddWord = this.onaddWord.bind(this);
-        this.ontoggleForm = this.ontoggleForm.bind(this);
-        this.onremoveword = this.onremoveword.bind(this);
-        this.ontoggleWord = this.ontoggleWord.bind(this);
-        this.onChangeFilter = this.onChangeFilter.bind(this);
-    }
-    onremoveword(id){
-        const words = this.state.words.filter(w => w.id !== id);
-        this.setState({words : words});
-    }
-    ontoggleWord(id){
-        const words = this.state.words.map(w => {
-            if(id !== w.id) return w;
-            return {...w,isMemorized : !w.isMemorized}
-        });
-        this.setState({words : words});
-    }
-    ontoggleForm(){
-        this.setState({shouldShowForm : !this.state.shouldShowForm});
-    }
-    onaddWord(new_word){
-        const new_words = words.concat(new_word);
-        this.setState({words : new_words  , shouldShowForm : false}); 
-    }
-    onChangeFilter(filterMode){
-        this.setState({filterMode});
-    }
-    get FilterWord(){
-        return(
-        this.state.words.filter(w => {
-            if(this.state.filterMode === 'Show_Memoried' && !w.isMemorized) return false; 
-            if(this.state.filterMode === 'Show_Forgot' && w.isMemorized) return false;
-            return true;    
-        }));
-       
-    }
-    render(){
-        const {filterMode , shouldShowForm} = this.state;
-        return(
-                <div>
-                {<Form 
-                    shouldShowForm={shouldShowForm} 
-                    ontoggleForm={this.ontoggleForm}
-                    onaddWord={this.onaddWord}/>}
-                <br/>
-                <Filter 
-                    filterMode={filterMode}
-                    onChangeFilter={this.onChangeFilter} />
-                {this.FilterWord.map(w => 
-                    <Word
-                         word={w} 
-                         key={w.id}
-                         onremoveword={this.onremoveword}
-                         ontoggleWord={this.ontoggleWord}/>)}
-            </div>
-        );
-    }
+    {id : 'a1' , en : 'One' , vn : 'Mot' , isMemorized : true},
+    {id : 'a2' , en : 'Two' , vn : 'Hai' , isMemorized : false},
+    {id : 'a3' , en : 'Three' , vn : 'Ba' , isMemorized : true}
+ ]
+export default class List extends Component {
+  constructor(props){
+    super(props);
+    this.state = {words  , shouldShowForm : false , filterMode : 'Show_All'}
+    this.ontoggleForm = this.ontoggleForm.bind(this);
+    this.onremoveWord = this.onremoveWord.bind(this);
+    this.ontoggleWord = this.ontoggleWord.bind(this);
+    this.onaddWord = this.onaddWord.bind(this);
+    this.onChangeFilterMode = this.onChangeFilterMode.bind(this);
+  }
+  onremoveWord(id){
+    const newWords = this.state.words.filter(w => w.id !== id);
+    this.setState({words :newWords});
+  }
+  ontoggleWord(id){
+    const newWords = this.state.words.map(w =>{
+      if(w.id !== id) return w;
+      // const {id , en , vn  } = word;
+      const newWord = {...w , isMemorized :!w.isMemorized}
+      return newWord;
+  })
+  this.setState({words : newWords});
+  }
+  ontoggleForm(){
+    this.setState({shouldShowForm : !this.state.shouldShowForm})
+  }
+  getFiltermode(){
+    const {words , filterMode} = this.state;
+    const newWords = words.filter(w => {
+      if(filterMode === 'Show_Forgot' && w.isMemorized) return false;
+      if(filterMode === 'Show_Memorized' && !w.isMemorized) return false;
+      return true;
+    })
+    return newWords
+  }
+  onaddWord(newWord){    
+    const newWords = this.state.words.concat(newWord);
+    this.setState({words : newWords , shouldShowForm : false });
+  }
+  onChangeFilterMode(filterMode){
+    this.setState({filterMode})
+  }
+  render() {
+    return (
+      <div>
+        {<Form 
+            shouldShowForm={this.state.shouldShowForm}
+            ontoggleForm={this.ontoggleForm}
+            onaddWord={this.onaddWord}
+        />}
+        {<Filter 
+            filterMode={this.state.filterMode}
+            onChangeFilterMode={this.onChangeFilterMode}/>}
+        {this.getFiltermode().map(word => 
+          <Word 
+            word={word} 
+            key={word.id}
+            onremoveWord={this.onremoveWord}
+            ontoggleWord={this.ontoggleWord}/>)}
+      </div>
+    )
+  }
 }
